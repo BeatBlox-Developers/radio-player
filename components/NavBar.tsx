994b77942558c508fb6bf42dcf51bfb02de3649b
nhussein11 <nicolashussein14@gmail.com>
2023-10-29 16:13:26 -0300

    fix: adding navbar

---
+++ components/NavBar.tsx
@@ -0,0 +1,10 @@
+export const NavBar = () => {
+  return (
+    <nav className="w-full flex justify-between items-center h-24 text-white px-12">
+      <img src="/beatblox-logo-iso.svg" alt="" className="h-7 mt-10 invert" />
+      <span className="bg-white rounded-3xl text-black py-2 px-4">
+        #testuser Counter
+      </span>
+    </nav>
+  );
+};
