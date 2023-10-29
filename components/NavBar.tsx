bd44a1dfc6a1fa0bbfa3e57e8a800b78deb75caa
nhussein11 <nicolashussein14@gmail.com>
2023-10-29 16:36:23 -0300

    fix: seconds played feature added

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,9 +1,13 @@
-export const NavBar = () => {
+type NavBarProps = {
+  secondsPlayed: number;
+};
+
+export const NavBar = ({ secondsPlayed }: NavBarProps) => {
   return (
     <nav className="w-full flex justify-between items-center h-24 text-white px-12">
       <img src="/beatblox-logo-iso.svg" alt="" className="h-7 mt-10 invert" />
       <span className="bg-white rounded-3xl text-black py-2 px-4">
-        #testuser Counter
+        #testuser {secondsPlayed}
       </span>
     </nav>
   );
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,16 @@
 import { NavBar } from "@/components/NavBar";
 import { useEffect, useRef, useState } from "react";
 import { AiOutlinePause } from "react-icons/ai";
+import { FiPlay } from "react-icons/fi";
 import WaveSurfer from "wavesurfer.js";
 
 export default function Home() {
   const [volume, setVolume] = useState(1); // Initial volume set to 1 (maximum)
+  const [isPlaying, setIsPlaying] = useState(false);
+  const [waveform, setWaveform] = useState(null); // Initial waveform set to null
+  const [secondsPlayed, setSecondsPlayed] = useState(0); // Initial seconds played set to 0
+  const videoRef = useRef(null);
+
   const waveformRef = useRef(null);
   useEffect(() => {
     const wavesurfer = WaveSurfer.create({
@@ -15,11 +21,13 @@ export default function Home() {
       barWidth: 3,
       height: 40,
     });
+    setWaveform(wavesurfer as any);
 
     wavesurfer.load("/video-mockup.mp4");
-    // wavesurfer.on("ready", function () {
-    //   wavesurfer.play();
-    // });
+    // Event listener for audioprocess to update secondsPlayed
+    wavesurfer.on("audioprocess", () => {
+      setSecondsPlayed(wavesurfer.getCurrentTime().toFixed(0));
+    });
 
     // Clean up the WaveSurfer instance when the component is unmounted
     return () => {
@@ -27,23 +35,47 @@ export default function Home() {
     };
   }, []); // Empty dependency array ensures that this effect runs once after the initial render
 
+  useEffect(() => {
+    if (waveform) {
+      waveform.setVolume(volume);
+    }
+  }, [volume]); // Run this effect whenever the volume changes
+
   const handleVolumeChange = (event: any) => {
+    console.log(event.target.value);
     const newVolume = parseFloat(event.target.value);
     setVolume(newVolume);
   };
 
+  const handlePlayPause = () => {
+    setIsPlaying(!isPlaying);
+    if (!isPlaying && waveform) {
+      waveform.play();
+      videoRef.current!.play();
+      return;
+    }
+    if (isPlaying && waveform) {
+      waveform.pause();
+      videoRef.current!.pause();
+      return;
+    }
+  };
+
   return (
     <main className="h-full w-screen bg-black">
-      <NavBar />
+      <NavBar secondsPlayed={secondsPlayed} />
       <div className="flex justify-center items-center h-full">
-        <video className="h-96 w-96 mt-0" autoPlay loop muted>
+        <video ref={videoRef} className="h-96 w-96 mt-0" loop muted>
           <source src="/video-mockup.mp4" type="video/mp4" />
         </video>
       </div>
       <div className="w-full flex  items-end justify-center  fixed bottom-5  text-white">
         <div className="w-fit py-3 px-8 border border-white rounded-[2.2rem] flex flex-row justify-between gap-10">
-          <button className="rounded-full bg-white text-black h-10 w-10 flex justify-center items-center ">
-            <AiOutlinePause size={24} />
+          <button
+            className="rounded-full bg-white text-black h-10 w-10 flex justify-center items-center"
+            onClick={handlePlayPause}
+          >
+            {isPlaying ? <AiOutlinePause size={24} /> : <FiPlay size={24} />}
           </button>
           <img
             src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTDnZwcezVPVhgI3TxA4DNNFbgBHgJTW6IP7g&usqp=CAU"
